640089e0daa5a590b266e853dc50e4808dfdc5b3
Juan Carlos Wu <jczas@sina.com>
2021-03-28 22:12:04 -0400

    :package: Extended prepublish script

--- src/__tests__/package-info.test.ts
+++
@@ -1,9 +0,0 @@
-import { getVersion, getDescription } from './../utils/package-info.util';
-
-test('Packge Info Util Test -- getVersion function', () => {
-  expect(getVersion()).toEqual(`${String.fromCodePoint(0x1f4e6)}Version: ${'0.0.1'}`);
-});
-
-test('Packge Info Util Test -- getDescription function', () => {
-  expect(getDescription()).toEqual(`${String.fromCodePoint(0x1f4e6)}Description: ${'Nativescript 7 theme builder'}`);
-});
